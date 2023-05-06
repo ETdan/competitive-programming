d1e7740554d4ae72efb841bbb6bf9102f9125f69
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-05-06 12:45:45 -0400

    Create DominoPiling.cpp

---
+++ DominoPiling.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int first,second;
+    cin>>first>>second;
+    first*=second;
+    first/=2;
+    cout<<first <<endl;
+    
+    return 0;
+}
