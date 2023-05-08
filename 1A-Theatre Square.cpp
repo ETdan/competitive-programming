9fdc0641fffdc9bab42a67c042d50a57506cdb94
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-05-08 16:03:54 -0400

    Create 1A-Theatre Square.cpp

---
+++ 1A-Theatre Square.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h> 
+using namespace std;
+
+int main(){
+    long double n,m,a;
+    cin>>n;
+    cin>>m;
+    cin>>a;
+    
+    long long int result=ceil(n/a)*ceil(m/a);
+    cout<<result;
+    
+    return 0;
+}
