63e9968898b41d4c45a25ac4f678dcb58f0ca93a
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-05-13 16:15:19 -0400

    Update 1A-Theatre Square.cpp

--- 1A-Theatre Square.cpp
+++ 1A-Theatre Square.cpp
@@ -1,15 +1,10 @@
 #include<iostream>
 #include<math.h> 
 using namespace std;
-
 int main(){
     long double n,m,a;
-    cin>>n;
-    cin>>m;
-    cin>>a;
-    
+    cin>>n;cin>>m;cin>>a;
     long long int result=ceil(n/a)*ceil(m/a);
     cout<<result;
-    
     return 0;
 }
