f270bd61f29d2f81476c856f22b67a445082ac62
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-05-15 14:56:03 -0400

    Create select.cpp

---
+++ select.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    void selectionSort(int arr[], int n)
+    {
+       for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n-1; j++)
+            {
+                if (arr[j]>arr[j+1])
+                    swap(arr[j],arr[j+1]);
+            }
+             
+        }
+    }
+};
