56d51fb2bf5a0121db8aadeda39dc9e5d5265a10
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-05-18 17:23:17 -0400

    Update How-Many-Numbers-Are-Smaller-Than-the-Current-Number.cpp

--- How-Many-Numbers-Are-Smaller-Than-the-Current-Number.cpp
+++ How-Many-Numbers-Are-Smaller-Than-the-Current-Number.cpp
@@ -6,12 +6,8 @@ public:
     for (int i = 0; i < nums.size(); i++)
     {
         for (int j = 0; j < nums.size(); j++)
-        {
             if (nums[i]>nums[j])
-            {
                 c++;
-            }
-        }
         n.push_back(c);
         c=0;
     }
