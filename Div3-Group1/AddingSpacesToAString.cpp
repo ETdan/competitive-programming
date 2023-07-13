8a64e1d86b8e071197bc45332edb6867e98598e3
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-07-13 03:51:32 -0400

    [question] (https://leetcode.com/problems/adding-spaces-to-a-string/description/)

---
+++ Div3-Group1/AddingSpacesToAString.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string answer="";
+        int j=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(j<spaces.size()&&i==spaces[j])
+            {
+                answer+=" ";
+                answer+=s[i];
+                j++;
+            }
+            else
+                answer+=s[i];
+        }
+        return answer;
+    }
+};
