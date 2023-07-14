e8267cac0295225787e405e74ea37ee64c53d545
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-07-14 12:06:00 -0400

    https://practice.geeksforgeeks.org/problems/selection-sort/1

---
+++ Div3-Group1/SelectionSort.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+    }
+     
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+       for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n-1; j++)
+            {
+                if (arr[j]>arr[j+1])
+                    swap(arr[j],arr[j+1]);
+            }
+             
+        }
+    }
+};
