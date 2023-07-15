387e5a31d765849ec7c8ddeb71939215f48ed2f4
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-07-15 06:50:24 -0400

    solve https://www.hackerrank.com/challenges/countingsort1/problem in py

--- Div3-Group1/CountingSort1.cpp
+++ Div3-Group1/CountingSort1.cpp
@@ -1,98 +1,36 @@
-#include <bits/stdc++.h>
+#!/bin/python3
 
-using namespace std;
+import math
+import os
+import random
+import re
+import sys
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
 
-/*
- * Complete the 'countingSort' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-vector<int> countingSort(vector<int> arr) {
-    vector<int> answer (100);
-    for(int i=0;i<arr.size();i++)
-        answer[arr[i]]++;
-    return answer;
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string n_temp;
-    getline(cin, n_temp);
-
-    int n = stoi(ltrim(rtrim(n_temp)));
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
-    }
-
-    vector<int> result = countingSort(arr);
-
-    for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
-        }
-    }
-
-    fout << "\n";
-
-    fout.close();
+def countingSort(arr):
+    # Write your code here
+    answer=[0] *100
+    for i in range(len(arr)):
+        answer[arr[i]]+=1
+    return answer
     
-    return 0;
-}
 
-string ltrim(const string &str) {
-    string s(str);
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
+    n = int(input().strip())
 
-    return s;
-}
+    arr = list(map(int, input().rstrip().split()))
 
-string rtrim(const string &str) {
-    string s(str);
+    result = countingSort(arr)
 
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
 
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-    tokens.push_back(str.substr(start));
-
-    return tokens;
-}
+    fptr.close()
