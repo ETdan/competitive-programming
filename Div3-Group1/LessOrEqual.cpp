3637bd9c0318547fb0af18266b84c7c4f6bc852c
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-07-16 08:09:45 -0400

    https://codeforces.com/problemset/problem/977/C

---
+++ Div3-Group1/LessOrEqual.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n=0;
+    int k=0;
+
+    cin>>n;
+    cin>>k;
+
+    vector<long int> b(n);
+
+    for (int i = 0; i < n; i++)
+        cin>>b[i];
+
+    sort(b.begin(),b.end());
+
+    if (k==n)
+        cout<< b[k-1]<<endl;
+    else
+    {
+        if(k==0)
+        {
+            if(b[0]-1>0)
+                cout<<b[0]-1<<endl;
+            else
+                cout<<-1<<endl;
+        }
+        else if (b[k]==b[k-1])
+            cout<< -1<<endl;
+        else
+            cout<< b[k-1]<<endl;
+    }
+  
+    n=0;
+    k=0;
+    b.clear();
+    return 0;
+}
+
