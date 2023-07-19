f2db886d4a87e42b5f93921a25e58fdd87c3678e
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-07-19 09:19:26 -0400

    https://leetcode.com/problems/palindrome-linked-list/description/

---
+++ Div3-Group1/PalindromeLinkedList.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        bool answer= true;
+        vector<int> container; 
+
+        ListNode* holder = head;
+
+        if(holder->next==NULL)
+        {
+            return true;
+        }
+        else
+        {
+            while(holder)
+            {
+                container.push_back(holder->val);
+                holder=holder->next;
+            }
+        }
+        
+        long long int start=0;
+        long long int end=container.size()-1;
+
+        while(start<end)
+        {
+            if(container[start]!=container[end])
+            {
+                answer=false;
+                break;
+            }   
+            else{
+                start++;
+                end--;
+            }
+        }
+        return answer;
+    }
+};
