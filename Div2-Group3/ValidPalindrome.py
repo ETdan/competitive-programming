6cd2bde46219e901b911be9bc9bbf5bff3e77fbb
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-09-13 10:26:45 -0400

    solve https://leetcode.com/problems/valid-palindrome/

--- Div2-Group3/ValidPalindrome.py
+++ Div2-Group3/ValidPalindrome.py
@@ -5,11 +5,32 @@ class Solution(object):
         :rtype: bool
         """
         word=""
-        for letter in s:
-            if letter.isalnum():
-                if letter.isdigit():
-                    word += letter
+        left=0
+        right=len(s)-1
+        while left < right:
+            a = s[left]
+            b = s[right]
+            print(a,b)
+            if a.isalnum() and b.isalnum():
+                if a.lower() == b.lower():
+                    left += 1
+                    right -= 1
+                    continue
                 else:
-                    word += letter.lower()
-        word_reverse=word[::-1]
-        return word == word_reverse
+                    return False
+            if not a.isalnum():
+                print(a)
+                left += 1
+            if not b.isalnum():
+                right -= 1
+        return True
+        # for letter in s:
+        #     if letter.isalnum():
+        #         if letter.isdigit():
+        #             word += letter
+        #         else:
+        #             word += letter.lower()
+
+        # word_reverse=word[::-1]
+        # return word == word_reverse
+        
