4fe460b7348bd3014c2ce3ba13086fd2a76f566f
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-11-29 15:42:49 -0600

    Added solution for Find the Maximum Achievable Number

---
+++ leet-code-solutions/find-the-maximum-achievable-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        """
+        :type num: int
+        :type t: int
+        :rtype: int
+        """
+
+        return num+t+(1*t)
+        
