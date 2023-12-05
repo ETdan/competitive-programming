6fefe4ad1491fb4d441a55eebece701b0e11caea
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-12-05 20:00:10 +0300

    Added solution for Non-decreasing Array

---
+++ leet-code-solutions/non-decreasing-array.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def isSorted(number):
+            return number == sorted(number)
+
+        modify=False
+
+        sorted_nums=sorted(nums)
+
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1] and not modify:
+                
+                if isSorted(nums[:i]+[nums[i+1]]+nums[i+1:]):
+                    print("first")
+                    modify=True
+                elif isSorted(nums[:i+1]+[nums[i]]+nums[i+2:]):
+                    print("second")
+                    modify=True
+                else:
+                    # print(nums[:i+1]+[nums[i]]+nums[i+2:])
+                    # print(nums[:i]+[nums[i+1]]+nums[i+1:])
+                    # print(sorted_nums)
+
+                    return False
+            
+        return True
+        
