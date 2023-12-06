903f3344b6b5f10b9a2cc380a9db6be1cb5591b3
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-12-06 17:14:30 +0300

    Added solution for Shuffle the Array

---
+++ leet-code-solutions/shuffle-the-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        n1=nums[:n]
+        n2=nums[n:]
+        answer=[]
+        
+        for i in range(len(nums)):
+            if i < len(n1):
+                answer.append(n1[i])
+            if i < len(n1):
+                answer.append(n2[i])
+        
+        return answer
+        
