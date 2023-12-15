6ee5d03e13ff368447dacbf08319a7642376d26e
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2023-12-15 19:38:24 +0300

    Added solution for Design Bitset

---
+++ leet-code-solutions/design-bitset.py
@@ -0,0 +1,99 @@
+class Bitset(object):
+
+    def __init__(self, size):
+        """
+        :type size: int
+        """
+        self.n=size
+        
+        self.bit0=[0]*self.n
+        self.bit1=[1]*self.n
+        
+        self.count_=0
+
+    def fix(self, idx):
+        """
+        :type idx: int
+        :rtype: None
+        """
+        
+        if self.bit0[idx] == 0:
+            self.bit0[idx] = 1
+            self.bit1[idx] = 0
+            self.count_+=1
+            
+
+    def unfix(self, idx):
+        """
+        :type idx: int
+        :rtype: None
+        """
+
+        # if self.first:
+        if self.bit0[idx] == 1:
+            self.bit0[idx]=0
+            self.bit1[idx]=1
+            if self.count_ > 0:
+                self.count_-=1
+            
+        
+
+    def flip(self):
+        """
+        :rtype: None
+        """
+        # print("fb",self.first)
+        self.bit0 ,self.bit1 = self.bit1 ,self.bit0
+        self.count_= self.n - self.count_
+        # print("fa",self.first)
+
+    def all(self):
+        """
+        :rtype: bool
+        """
+        # print(self.first)
+
+        if self.count_ == self.n:
+            return True
+        else:
+            return False
+
+    def one(self):
+        """
+        :rtype: bool
+        """
+        if self.count_ >0:
+            return True
+        else:
+            return False
+                
+                
+        
+
+    def count(self):
+        """
+        :rtype: int
+        """
+        # print(self.first)
+        # if self.first:
+        return self.count_
+        # else:
+            # return self.count_
+
+    def toString(self):
+        """
+        :rtype: str
+        """
+        return "".join([str(bit) for bit in self.bit0])
+
+        # return "".join([str(bit) for bit in self.bit1])
+
+# Your Bitset object will be instantiated and called as such:
+# obj = Bitset(size)
+# obj.fix(idx)
+# obj.unfix(idx)
+# obj.flip()
+# param_4 = obj.all()
+# param_5 = obj.one()
+# param_6 = obj.count_()
+# param_7 = obj.toString()
