2e0e32c2ffb092dd4a50c9ffb2a7d9b1399b66a9
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-02-22 09:33:50 +0300

    Add solution for Max Increase to Keep City Skyline

---
+++ leet-code-solutions/leetcode/max-increase-to-keep-city-skyline.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        row_max = [0] * len(grid)
+        col_max = [0] * len(grid[0])
+
+        for i, row in enumerate(grid):
+            row_max[i] = max(row)
+
+        for i in range(len(grid)):
+            max_ = 0
+            for j in range(len(grid[0])):
+                if max_ < grid[j][i]:
+                    max_ = grid[j][i]
+                # print(grid[j][i])
+            col_max[i] = max_
+        # print(row_max,col_max)
+        answer=0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                max_ = min(row_max[i],col_max[j])
+                answer += max_ - grid[i][j]
+        
+        return answer
