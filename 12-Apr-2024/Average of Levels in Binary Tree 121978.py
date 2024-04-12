f2aefc1bd6fdfdce7f0c1607225426b25ba8e1ab
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-04-12 18:01:16 -0400

    Created 'Average of Levels in Binary Tree' by A2SV Hub

---
+++ 12-Apr-2024/Average of Levels in Binary Tree 121978.py
@@ -0,0 +1,29 @@
+# Problem: Average of Levels in Binary Tree - https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        q=deque()
+        q.append(root)
+
+        answer=[]
+
+        while q:
+            temp = 0
+            length = len(q)
+            for _ in range(length):
+                node = q.popleft()
+                temp += node.val
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            answer.append(temp/length)
+        
+        return answer
