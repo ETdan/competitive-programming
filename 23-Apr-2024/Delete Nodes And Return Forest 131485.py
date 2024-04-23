73198e6422acdd4a500341b6a0d3c76048a454e4
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-04-23 16:28:33 -0400

    Created 'Delete Nodes And Return Forest' by A2SV Hub

---
+++ 23-Apr-2024/Delete Nodes And Return Forest 131485.py
@@ -0,0 +1,29 @@
+# Problem: Delete Nodes And Return Forest - https://leetcode.com/problems/delete-nodes-and-return-forest/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        result = []
+        to_delete_set = set(to_delete)
+
+        def dfs(node, is_root):
+            if not node:
+                return None
+
+            is_deleted = node.val in to_delete_set
+
+            if is_root and not is_deleted:
+                result.append(node)
+
+            node.left = dfs(node.left, is_deleted)
+            node.right = dfs(node.right, is_deleted)
+
+            return None if is_deleted else node
+
+        dfs(root, True)
+        return result
