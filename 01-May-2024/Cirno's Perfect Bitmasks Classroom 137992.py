7223ca00a0caaa97df00688d17cd631bb4a7e3c1
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-05-01 10:34:18 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 01-May-2024/Cirno's Perfect Bitmasks Classroom 137992.py
@@ -0,0 +1,26 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+import sys
+input = sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    bitn = ['0']
+    bitn.extend(list(bin(n)[2:]))
+    answer = ['0']*len(bitn)
+    for i in range(len(bitn)-1, -1, -1):
+        if bitn[i] == '1':
+            answer[i] = '1'
+            break
+
+    if int(''.join(answer), 2) ^ int(''.join(bitn), 2) != 0:
+        print(int(''.join(answer), 2))
+    else:
+        for i in range(len(bitn)-1, -1, -1):
+            if bitn[i] == '0':
+                answer[i] = '1'
+                break
+        print(int(''.join(answer), 2))
+        # print(bitn)
+        # print(answer)
