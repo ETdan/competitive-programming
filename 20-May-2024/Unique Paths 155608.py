b52292de087edc29757be246dbdbe58c57a9097c
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-05-20 16:38:04 -0400

    Created 'Unique Paths' by A2SV Hub

---
+++ 20-May-2024/Unique Paths 155608.py
@@ -0,0 +1,12 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        oldrow = [1]*n
+        for i in range(m-1):
+            newrow = [1]*n
+            for j in range(n-2, -1, -1):
+                newrow[j] = newrow[j+1] + oldrow[j]
+            oldrow = newrow
+        return oldrow[0]
+        
