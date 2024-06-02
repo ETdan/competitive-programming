8d85a649f48a9a93a0c5a7f0dde3ee08c731ea2e
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-06-02 16:48:09 -0400

    Created 'Maximal Square' by A2SV Hub

---
+++ 02-Jun-2024/Maximal Square 163440.py
@@ -0,0 +1,26 @@
+# Problem: Maximal Square - https://leetcode.com/problems/maximal-square/
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        
+        memo = {}
+        
+        def dp(row, col):
+            if row == len(matrix) or col == len(matrix[0]):
+                return 0
+        
+            if (row, col) not in memo:
+                toDiagonal = dp(row + 1, col + 1)            
+                toBottom = dp(row + 1, col)
+                toRight = dp(row, col + 1)
+                
+                if matrix[row][col] == "1":
+                    memo[(row, col)] = 1 + min(toRight, toBottom, toDiagonal)
+                else:
+                    memo[(row, col)] = 0
+
+            return memo[(row, col)]
+        
+        dp(0,0)
+        
+        return max(memo.values()) ** 2
