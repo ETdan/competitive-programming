4663cd61809894d439262e680bb87f7e3a0bc482
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-07-16 22:54:34 +0300

    Created 'Escape The Ghosts' by A2SV Hub

---
+++ 16-Jul-2024/Escape The Ghosts 181027.py
@@ -0,0 +1,23 @@
+# Problem: Escape The Ghosts - https://leetcode.com/problems/escape-the-ghosts/
+
+class Solution(object):
+    def escapeGhosts(self, ghosts, target):
+        """
+        :type ghosts: List[List[int]]
+        :type target: List[int]
+        :rtype: bool
+        """
+        distance_to_goal=(abs(target[0])+abs(target[1]))
+        x2=target[0]
+        y2=target[1]
+        
+        for i in range(len(ghosts)):
+            x1=ghosts[i][0]
+            y1=ghosts[i][1]
+            x=abs(x2-x1)
+            y=abs(y2-y1)
+            
+            if distance_to_goal >= x + y:
+                return False
+
+        return True
