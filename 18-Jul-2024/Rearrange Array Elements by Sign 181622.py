ba8e1ebfe1783e618cec9b32946b4ab8400f452c
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-07-18 22:08:43 +0300

    Created 'Rearrange Array Elements by Sign' by A2SV Hub

---
+++ 18-Jul-2024/Rearrange Array Elements by Sign 181622.py
@@ -0,0 +1,23 @@
+# Problem: Rearrange Array Elements by Sign - https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
+
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        posetive_list=[]
+        negative_list=[]
+        answer=[]
+
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                negative_list.append(nums[i])
+            else:
+                posetive_list.append(nums[i])
+        
+        for i in range(len(posetive_list)):
+            answer.append(posetive_list[i])
+            answer.append(negative_list[i])
+            
+        return answer
