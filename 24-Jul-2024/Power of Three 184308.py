b7fd806eee5fbef785f3d25acac07f904b4e4101
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-07-24 22:45:48 +0300

    Created 'Power of Three' by A2SV Hub

---
+++ 24-Jul-2024/Power of Three 184308.py
@@ -0,0 +1,10 @@
+# Problem: Power of Three - https://leetcode.com/problems/power-of-three/
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n ==1:
+            return True
+        if n == 0:
+            return False
+        else:
+            return self.isPowerOfThree(n/3) and True
