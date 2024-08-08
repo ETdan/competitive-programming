2a901f7b6afa477a3f08f9001d86f95c172d0765
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-08-08 23:12:06 +0300

    Created 'Minimum Number of Pushes to Type Word II' by A2SV Hub

---
+++ 08-Aug-2024/Minimum Number of Pushes to Type Word II 188591.py
@@ -0,0 +1,17 @@
+# Problem: Minimum Number of Pushes to Type Word II - https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+
+class Solution:
+    def minimumPushes(self, input_text: str) -> int:
+        letter_counts = [0] * 26
+        for char in input_text:
+            letter_counts[ord(char) - ord('a')] += 1
+        
+        sorted_counts = sorted(letter_counts, reverse=True)
+        
+        total_key_presses = 0
+        for index, count in enumerate(sorted_counts):
+            if count == 0:
+                break
+            total_key_presses += (index // 8 + 1) * count
+        
+        return total_key_presses
