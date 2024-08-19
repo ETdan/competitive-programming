f5344293c5469cfaf6dac64300e3e443858c616c
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-08-19 23:38:27 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 19-Aug-2024/Mod Power 198037.py
@@ -0,0 +1,8 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+m=int(input())
+print(pow(a,b))
+print(pow(a,b,m))
