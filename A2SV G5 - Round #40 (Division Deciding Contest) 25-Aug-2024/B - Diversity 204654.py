fb2cca9026f75fabfc79ec071e89e2e59fe1b031
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-08-25 21:56:45 +0300

    Created 'B - Diversity' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 25-Aug-2024/B - Diversity 204654.py
@@ -0,0 +1,22 @@
+# Problem: B - Diversity - https://codeforces.com/gym/543431/problem/B
+
+import sys
+from collections import Counter
+input = sys.stdin.readline
+s = input().strip()
+k = int(input())
+length = len(s)
+
+uniq = Counter(s)
+count = 0
+for key, val in uniq.items():
+    if val == 1:
+        count += 1
+if length < k or k > 26:
+    print("impossible")
+else:
+    dif = set(s)
+    if len(dif) >= k:
+        print(0)
+    else:
+        print(k-len(dif))
