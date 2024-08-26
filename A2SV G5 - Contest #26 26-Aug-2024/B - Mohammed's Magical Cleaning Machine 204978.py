db3d44c44e7a15538cf8ee81df5fd305c6e25d03
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-08-26 23:18:50 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 26-Aug-2024/B - Mohammed's Magical Cleaning Machine 204978.py
@@ -0,0 +1,19 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    counter = 0
+    found_num = False
+    for i in range(n-1):
+        if arr[i] == 0 and found_num:
+            counter += 1
+        elif arr[i] > 0:
+            found_num = True
+            counter += arr[i]
+    print(counter)
