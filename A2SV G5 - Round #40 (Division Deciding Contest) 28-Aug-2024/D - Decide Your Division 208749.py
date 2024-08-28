234eae92b7ecc8a6e12287595dd2ee75f9798277
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-08-28 22:26:51 +0300

    Created 'D - Decide Your Division' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 28-Aug-2024/D - Decide Your Division 208749.py
@@ -0,0 +1,22 @@
+# Problem: D - Decide Your Division - https://codeforces.com/gym/543431/problem/D
+
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    count = 0
+    while n > 1:
+        if n % 5 == 0:
+            n = (n*4) // 5
+        elif n % 3 == 0:
+            n = (n*2) // 3
+        elif n % 2 == 0:
+            n //= 2
+        else:
+            n = 0
+        count += 1
+    if n == 1:
+        print(count)
+    else:
+        print(-1)
