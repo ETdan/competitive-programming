3154a27d336f1acc450339e4a80f8d24023da58c
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-08-29 23:48:36 +0300

    Created 'C - Benches' by A2SV Hub

---
+++ A2SV G5 - Contest #28 29-Aug-2024/C - Benches 209244.py
@@ -0,0 +1,27 @@
+# Problem: C - Benches - https://codeforces.com/gym/540354/problem/C
+
+import sys
+import math
+input = sys.stdin.readline
+t = int(input())
+m = int(input())
+km = m
+arr = []
+for _ in range(t):
+    ai = int(input())
+    arr.append(ai)
+
+arr.sort()
+max_ = arr[-1]
+i = 0
+
+while i < len(arr) and arr[i] <= max_ and m:
+    if m > max_-arr[i]:
+        m -= max_-arr[i]
+    else:
+        m = 0
+    i += 1
+
+min_ = max_+math.ceil(m / t)
+
+print(min_, max_+km)
