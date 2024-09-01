8859918ab515d12aaf1d51ee7059c355f1bf1291
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-09-01 21:22:32 +0300

    Created 'C - InXtracting a Subsegment' by A2SV Hub

---
+++ A2SV G5 - Contest #26 01-Sep-2024/C - InXtracting a Subsegment 210170.py
@@ -0,0 +1,16 @@
+# Problem: C - InXtracting a Subsegment - https://codeforces.com/gym/537362/problem/C
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+if k == 1:
+    print(min(a))
+
+elif k == 2:
+    print(max(a[0], a[-1]))
+
+else:
+    print(max(a))
