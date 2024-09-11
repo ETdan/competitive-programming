93f692c44ac235d680eab16cb7fbf5bb6ddd370d
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-09-11 22:52:50 +0300

    Created 'A - Duale String' by A2SV Hub

---
+++ A2SV G5 - Contest #14 11-Sep-2024/A - Duale String 214414.py
@@ -0,0 +1,13 @@
+# Problem: A - Duale String - https://codeforces.com/gym/520688/problem/A
+
+from sys import stdin, stdout, setrecursionlimit
+import sys
+input = sys.stdin.readline
+t = int(input())
+
+for _ in range(t):
+    s = input().strip()
+    if 2*s[:len(s)//2] == s:
+        print("YES")
+    else:
+        print("NO")
