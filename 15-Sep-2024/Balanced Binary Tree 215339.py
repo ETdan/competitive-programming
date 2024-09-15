0797506d5ea6568506dfa8e655325b287e3ac9df
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-09-15 22:19:53 +0300

    Created 'Balanced Binary Tree' by A2SV Hub

---
+++ 15-Sep-2024/Balanced Binary Tree 215339.py
@@ -0,0 +1,21 @@
+# Problem: Balanced Binary Tree - https://leetcode.com/problems/balanced-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return (self.Height(root) >= 0)
+
+    def Height(self, root):
+        if root is None:  
+            return 0
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
+        
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:
+            return -1
+        
+        return max(leftheight, rightheight) + 1
