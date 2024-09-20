fb3ac35c3169a382ec5a62a5b219bf4968943a1e
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-09-20 23:15:28 +0300

    Created 'B - Excluded Integer Sum Problem' by A2SV Hub

---
+++ A2SV G5 - Contest #22 20-Sep-2024/B - Excluded Integer Sum Problem 216123.py
@@ -0,0 +1,17 @@
+# Problem: B - Excluded Integer Sum Problem - https://codeforces.com/gym/531455/problem/B
+
+import sys
+for _ in range(int(input())):
+    n, k, x = map(int, sys.stdin.readline().strip().split())
+    if x != 1:
+    	print("YES")
+    	print(n)
+    	print(*([1] * n))
+    elif k == 1 or (k == 2 and n % 2):
+    	print("NO")
+
+    else:
+    	print("YES")
+    	print(n // 2)
+    		
+    	print(*([2] * (n // 2 - 1)) + [3 if n % 2 == 1 else 2])
