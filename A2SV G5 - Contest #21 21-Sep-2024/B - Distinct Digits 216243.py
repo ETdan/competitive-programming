92f848d817e1b2ff46baf4af53454f8ff8f33b24
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-09-21 22:49:58 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 21-Sep-2024/B - Distinct Digits 216243.py
@@ -0,0 +1,14 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+
+    ans = []
+    for digit in range(9, 0, -1):
+        if n >= digit:
+            ans.append(digit)
+            n -= digit
+    ans.reverse()
+    print(*ans, sep="")
