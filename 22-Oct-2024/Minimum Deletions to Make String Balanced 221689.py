0e9ade5fd110d57a12689a9927a86ad608375a24
Daniel Asfaw <114868325+ETdan@users.noreply.github.com>
2024-10-22 22:36:29 +0300

    Created 'Minimum Deletions to Make String Balanced' by A2SV Hub

---
+++ 22-Oct-2024/Minimum Deletions to Make String Balanced 221689.py
@@ -0,0 +1,14 @@
+# Problem: Minimum Deletions to Make String Balanced - https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/
+
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        n = len(s)
+        f = [0] * (n + 1)
+        b = 0
+        for i, c in enumerate(s, 1):
+            if c == 'b':
+                f[i] = f[i - 1]
+                b += 1
+            else:
+                f[i] = min(f[i - 1] + 1, b)
+        return f[n]
